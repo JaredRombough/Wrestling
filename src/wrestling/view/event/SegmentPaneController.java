961c56f7626e13baf6615e1caabd2f567aef4eb6
JaredRombough <jared.rombough@gmail.com>
2018-03-04 17:34:24 -0600

    Angles
    Preliminary work on angles
    Add X button to remove specific teams from segment

--- src/wrestling/view/event/SegmentPaneController.java
+++ src/wrestling/view/event/SegmentPaneController.java
@@ -11,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
@@ -28,71 +29,77 @@ import wrestling.view.utility.ViewUtils;
 import wrestling.view.utility.interfaces.ControllerBase;
 
 public class SegmentPaneController extends ControllerBase implements Initializable {
-
+    
     private static final int DEFAULTTEAMS = 2;
-
+    
     @FXML
     private VBox teamsPane;
-
+    
     @FXML
     private Button addTeamButton;
-
+    
     @FXML
     private Button removeTeamButton;
-
+    
     @FXML
     private ComboBox matchRules;
-
+    
     @FXML
     private ComboBox matchFinishes;
-
+    
+    @FXML
+    private Button matchButton;
+    
+    @FXML
+    private Button angleButton;
+    
     private List<Screen> teamPaneWrappers;
-
+    
     private EventScreenController eventScreenController;
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         logger = LogManager.getLogger(this.getClass());
         teamPaneWrappers = new ArrayList<>();
-
+        
     }
-
+    
     @Override
     public void initializeMore() {
-
+        
         for (int i = 0; i < DEFAULTTEAMS; i++) {
-
+            
             addTeam();
-
+            
         }
-
+        
         intitializeMatchFinshesCombobox();
-
+        
         initializeMatchRulesCombobox();
-
+        
         updateMatchRulesCombobox();
-
+        
     }
-
+    
     private void intitializeMatchFinshesCombobox() {
-
+        
         matchFinishes.getSelectionModel().selectedItemProperty().addListener((ObservableValue observable, Object oldValue, Object newValue) -> {
             if (newValue != null) {
                 //only update when a new value is being selected
                 updateLabels();
             }
-
+            
         });
         //set the list cells display
         matchFinishes.setCellFactory(new Callback<ListView<MatchFinishes>, ListCell<MatchFinishes>>() {
             @Override
             public ListCell<MatchFinishes> call(ListView<MatchFinishes> p) {
                 return new ListCell<MatchFinishes>() {
-
+                    
                     @Override
                     protected void updateItem(MatchFinishes item, boolean empty) {
                         super.updateItem(item, empty);
-
+                        
                         if (item == null || empty) {
                             setText(null);
                         } else {
@@ -116,16 +123,16 @@ public class SegmentPaneController extends ControllerBase implements Initializab
                 }
             }
         });
-
+        
     }
-
+    
     private void initializeMatchRulesCombobox() {
 
         //for when selection changes
         matchRules.setOnAction(event -> {
             updateMatchRulesCombobox();
             updateLabels();
-
+            
         });
 
         //set the items, here we could filter out rules that the promotion can't use
@@ -136,11 +143,11 @@ public class SegmentPaneController extends ControllerBase implements Initializab
             @Override
             public ListCell<MatchRules> call(ListView<MatchRules> p) {
                 return new ListCell<MatchRules>() {
-
+                    
                     @Override
                     protected void updateItem(MatchRules item, boolean empty) {
                         super.updateItem(item, empty);
-
+                        
                         if (item == null || empty) {
                             setText(null);
                         } else {
@@ -164,24 +171,24 @@ public class SegmentPaneController extends ControllerBase implements Initializab
                 }
             }
         });
-
+        
         matchRules.getSelectionModel().selectFirst();
-
+        
     }
-
+    
     private void updateMatchRulesCombobox() {
         MatchRules current = (MatchRules) matchRules.getSelectionModel().getSelectedItem();
         MatchFinishes lastFinish = (MatchFinishes) matchFinishes.getSelectionModel().getSelectedItem();
         List<MatchFinishes> finishes = new ArrayList<>();
         for (MatchFinishes f : MatchFinishes.values()) {
-
+            
             if (current.nodq() && f.nodq()) {
                 finishes.add(f);
             } else if (!current.nodq()) {
                 finishes.add(f);
             }
         }
-
+        
         matchFinishes.setItems(FXCollections.observableArrayList(finishes));
         if (matchFinishes.getItems().contains(lastFinish)) {
             matchFinishes.getSelectionModel().select(lastFinish);
@@ -189,22 +196,27 @@ public class SegmentPaneController extends ControllerBase implements Initializab
             matchFinishes.getSelectionModel().selectFirst();
         }
     }
-
+    
     public void setEventScreenController(EventScreenController eventScreenController) {
         this.eventScreenController = eventScreenController;
-
+        
     }
-
+    
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
-
+        
         if (event.getSource() == addTeamButton) {
-
             addTeam();
-
         } else if (event.getSource() == removeTeamButton) {
             removeTeam();
-
+        } else if (event.getSource() == matchButton) {
+            for (Screen screen : teamPaneWrappers) {
+                ((TeamPaneWrapper) screen.controller).setMatch();
+            }
+        } else if (event.getSource() == angleButton) {
+            for (Screen screen : teamPaneWrappers) {
+                ((TeamPaneWrapper) screen.controller).setAngle();
+            }
         }
     }
 
@@ -216,7 +228,7 @@ public class SegmentPaneController extends ControllerBase implements Initializab
             ((TeamPaneWrapper) screen.controller).getTeamPaneController().removeWorker(worker);
         }
     }
-
+    
     public List<Worker> getWorkers() {
         List<Worker> workers = new ArrayList<>();
         for (Screen screen : teamPaneWrappers) {
@@ -224,48 +236,58 @@ public class SegmentPaneController extends ControllerBase implements Initializab
         }
         return workers;
     }
-
+    
     private void addTeam() {
-
+        
         Screen teamPaneWrapper = ViewUtils.loadScreenFromResource(ScreenCode.TEAM_PANE_WRAPPER, mainApp, gameController);
-
+        
         teamPaneWrappers.add(teamPaneWrapper);
-
+        
         TeamPaneController controller = ((TeamPaneWrapper) teamPaneWrapper.controller).getTeamPaneController();
         controller.setDragDropHandler(this, eventScreenController);
         controller.setTeamNumber(teamPaneWrappers.size() - 1);
-
+        ((TeamPaneWrapper) teamPaneWrapper.controller).getXButton().setOnAction(e -> removeTeam(teamPaneWrapper));
+        
         teamsPane.getChildren().add(teamPaneWrapper.pane);
         updateTeamNames();
-
+        
         eventScreenController.updateSegments();
-
+        
     }
-
+    
     @Override
     public void updateLabels() {
         for (Screen screen : teamPaneWrappers) {
             screen.controller.updateLabels();
-
+            
         }
         eventScreenController.updateSegments();
     }
-
+    
     private void removeTeam() {
 
         //we may want an alternate method where a minimum of one team is kept
         if (!teamPaneWrappers.isEmpty()) {
-
+            
             teamPaneWrappers.remove(teamPaneWrappers.size() - 1);
-
+            
             teamsPane.getChildren().remove(teamsPane.getChildren().size() - 1);
 
             //tell the event screen to update particularly the segment listView
             //because we have changed the segment name
             eventScreenController.updateSegments();
-
+            
         }
-
+        
+    }
+    
+    private void removeTeam(Screen teamPaneWrapper) {
+        if (teamsPane.getChildren().contains(teamPaneWrapper.pane)) {
+            teamsPane.getChildren().remove(teamPaneWrapper.pane);
+        }
+        if (teamPaneWrappers.contains(teamPaneWrapper)) {
+            teamPaneWrappers.remove(teamPaneWrapper);
+        }
     }
 
     //called by a team pane from drag dropped to update the team priority
@@ -274,7 +296,7 @@ public class SegmentPaneController extends ControllerBase implements Initializab
         //tell the event screen to update the event to reflect the new team priorirty
         eventScreenController.updateSegments();
         updateTeamNames();
-
+        
     }
 
     //this will need to be more complex when more types of segments are added
@@ -290,10 +312,10 @@ public class SegmentPaneController extends ControllerBase implements Initializab
                     break;
             }
             ((TeamPaneWrapper) screen.controller).getTeamPaneController().setTeamNameLabel(teamName);
-
+            
         }
     }
-
+    
     public SegmentView getSegmentView() {
         //this would return whatever segment we generate, match or angle
         //along with all the rules etc
@@ -311,21 +333,21 @@ public class SegmentPaneController extends ControllerBase implements Initializab
         while (!teamPaneWrappers.isEmpty()) {
             removeTeam();
         }
-
+        
         for (int i = 0; i < DEFAULTTEAMS; i++) {
             addTeam();
         }
-
+        
     }
-
+    
     private List<List<Worker>> getTeams() {
-
+        
         List<List<Worker>> teams = new ArrayList<>();
-
+        
         for (Screen screen : teamPaneWrappers) {
             teams.add(((TeamPaneWrapper) screen.controller).getTeamPaneController().getWorkers());
         }
-
+        
         return teams;
     }
 }
--- src/wrestling/view/event/TeamPaneWrapper.java
+++ src/wrestling/view/event/TeamPaneWrapper.java
@@ -1,21 +1,29 @@
 package wrestling.view.event;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import wrestling.view.utility.Screen;
@@ -31,10 +39,153 @@ public class TeamPaneWrapper extends ControllerBase implements Initializable {
     @FXML
     private VBox vBox;
 
+    @FXML
+    private HBox header;
+
+    @FXML
+    private Button xButton;
+
     private Screen teamPane;
     private SegmentPaneController segmentPaneController;
     private static final String TAB_DRAG_KEY = "anchorpane";
     private ObjectProperty<AnchorPane> draggingTab;
+
+    private ComboBox angleComboBox;
+    private ComboBox promoComboBox;
+    private ComboBox violenceComboBox;
+
+    private TeamPaneState currentState;
+
+    private enum TeamPaneState {
+        CHALLENGER,
+        CHALLENGED,
+        INTERFERENCE,
+        INTERVIEWER
+    }
+
+    private void setCurrentState(TeamPaneState state) {
+        currentState = state;
+        switch (currentState) {
+            case CHALLENGER:
+                break;
+            case CHALLENGED:
+                break;
+            case INTERFERENCE:
+                break;
+            case INTERVIEWER:
+                break;
+        }
+    }
+    
+    public Button getXButton() {
+        return xButton;
+    }
+
+    public void setMatch() {
+        vBox.getChildren().retainAll(teamPane.pane, header);
+    }
+
+    public void setAngle() {
+        vBox.getChildren().retainAll(teamPane.pane, header);
+        addAngleComboBox();
+        addViolenceComboBox();
+
+    }
+
+    public void setInterference() {
+        vBox.getChildren().retainAll(teamPane.pane, header);
+
+    }
+
+    private void addAngleComboBox() {
+        angleComboBox = new ComboBox();
+        angleComboBox.getItems().addAll(
+                "Promo",
+                "Offer",
+                "Challenge",
+                "Announcement");
+
+        vBox.getChildren().add(angleComboBox);
+        angleComboBox.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue ov, String t, String t1) {
+                if (t1 != null) {
+                    clearControls();
+                    if (t1.equals("Promo")) {
+                        addPromoComboBox();
+                    } else if (t1.equals("Offer")) {
+                        addJoinTeamComboBox();
+                    } else if (t1.equals("Challenge")) {
+                        addShowComboBox();
+                    }
+                    if(violenceComboBox != null && vBox.getChildren().contains(violenceComboBox))
+                    violenceComboBox.toFront();
+                }
+            }
+        });
+        angleComboBox.getSelectionModel().selectFirst();
+
+    }
+
+    private void clearControls() {
+        vBox.getChildren().retainAll(Arrays.asList(angleComboBox, teamPane.pane, violenceComboBox, header));
+    }
+
+    private void addPromoComboBox() {
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().addAll(
+                "Self Hype", "Target");
+        vBox.getChildren().add(comboBox);
+        comboBox.getSelectionModel().selectFirst();
+    }
+
+    private void addJoinTeamComboBox() {
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().addAll(
+                "New Tag Team", "New Stable", "Existing Stable");
+        vBox.getChildren().add(comboBox);
+        comboBox.getSelectionModel().selectFirst();
+    }
+
+    private void addShowComboBox() {
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().addAll(
+                "Tonight", "Next Show", "Next Big Show", "Show x");
+        vBox.getChildren().add(comboBox);
+        comboBox.getSelectionModel().selectFirst();
+    }
+
+    private void addViolenceComboBox() {
+        violenceComboBox = new ComboBox();
+        violenceComboBox.getItems().addAll(
+                "No Bump", "Attack", "Defend");
+        vBox.getChildren().add(violenceComboBox);
+        violenceComboBox.getSelectionModel().selectFirst();
+    }
+
+    private void addPresentComboBox() {
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().addAll(
+                "Present", "Not Present");
+        vBox.getChildren().add(comboBox);
+        comboBox.getSelectionModel().selectFirst();
+    }
+
+    private void successComboBox() {
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().addAll(
+                "Win", "Lose", "Draw");
+        vBox.getChildren().add(comboBox);
+        comboBox.getSelectionModel().selectFirst();
+    }
+
+    private void targetComboBox() {
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().addAll(
+                "Target A", "Target B", "Target C");
+        vBox.getChildren().add(comboBox);
+        comboBox.getSelectionModel().selectFirst();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -95,9 +246,9 @@ public class TeamPaneWrapper extends ControllerBase implements Initializable {
             event.consume();
         });
     }
-    
+
     public TeamPaneController getTeamPaneController() {
-        return (TeamPaneController)teamPane.controller;
+        return (TeamPaneController) teamPane.controller;
     }
 
 }
