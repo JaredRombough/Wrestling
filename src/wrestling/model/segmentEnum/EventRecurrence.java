187403b77703d9ea5ade48b35648d977e51311c1
JaredRombough <jared.rombough@gmail.com>
2018-04-30 13:05:38 -0500

    event template enum strings

--- src/wrestling/model/segmentEnum/EventRecurrence.java
+++ src/wrestling/model/segmentEnum/EventRecurrence.java
@@ -1,6 +1,28 @@
 package wrestling.model.segmentEnum;
 
-public enum EventRecurrence {
-    UNLIMITED,
-    LIMITED
+import wrestling.model.interfaces.Description;
+
+public enum EventRecurrence implements Description {
+    UNLIMITED {
+        @Override
+        public String description() {
+            return "Unlimited";
+        }
+    },
+    LIMITED {
+        @Override
+        public String description() {
+            return "Limited";
+        }
+    };
+
+    @Override
+    public String toString() {
+        return description();
+    }
+
+    @Override
+    public String result() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
